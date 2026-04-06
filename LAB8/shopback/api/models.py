496390bcb1bc57320de4cc9f509a86e429cacd3d
Alua <aluuua9@gmail.com>
2026-04-06 15:43:35 +0500

    LAB9 changes

--- LAB8/shopback/api/models.py
+++ LAB8/shopback/api/models.py
@@ -1,30 +1,18 @@
 from django.db import models
 
-# Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=255)
-    
-    def to_json(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            }
-        
+    name=models.CharField(max_length=255)
+
+def __str__(self):
+    return self.name
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.FloatField()
     description = models.TextField()
     count = models.IntegerField()
-    is_active = models.BooleanField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    
-    def to_json(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'price': self.price,
-            'description': self.description,
-            'count': self.count,
-            'is_active': self.is_active,
-            'category_id': self.category.id
-            }
+    is_active = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+
+    def __str__(self):
+        return self.name
--- LAB8/shopback/api/views.py
+++ LAB8/shopback/api/views.py
@@ -1,34 +1,21 @@
-from django.shortcuts import render
-from api.models import Category, Product
-from django.http import JsonResponse
+from rest_framework import viewsets
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-# Create your views here.
-def product_list(request):
-    products = Product.objects.all()
-    products_json = [p.to_json() for p in products]
-    return JsonResponse(products_json, safe = False)
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
 
-def product_detail(request, id):
-    try:
-        product = Product.objects.get(id = id)
-        return JsonResponse(product.to_json())
-    except Product.DoesNotExist:
-        return JsonResponse({'error': 'Product not found'}, status = 404)
+    @action(detail=True, methods=['get'])
+    def products(self, request, pk=None):
+        category = self.get_object()
+        products = Product.objects.filter(category=category)
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
 
-def category_list(request):
-    categories = Category.objects.all()
-    categories_json = [c.to_json() for c in categories]
-    return JsonResponse(categories_json, safe = False)
 
-def category_detail(request,id):
-    try:
-        category = Category.objects.get(id=id)
-        return JsonResponse(category.to_json())
-    except Category.DoesNotExist:
-        return JsonResponse({'error': 'Category not found'}, status=404)
-    
-def category_products(request,id):
-    products = Product.objects.filter(category_id = id)
-    products_json = [p.to_json() for p in products]
-    return JsonResponse(products_json, safe = False)
-
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
